b990dab27be09e3c4944568f66d4164fd034a76b
Andrey Dolgikh <a.dolgikh@evrone.team>
2025-02-23 19:21:15 +0300

    init

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
-require_relative "boot"
+# frozen_string_literal: true
 
-require "rails/all"
+require_relative 'boot'
+
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,7 +17,13 @@ module MerchbotBackend
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
+    config.hosts << ENV['BACKEND_URL']&.gsub('https://', '')
+    config.eager_load_paths << Rails.root.join('lib')
+    config.generators do |g|
+      g.test_framework :rspec, fixture: false
+      g.view_specs      false
+      g.helper_specs    false
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
